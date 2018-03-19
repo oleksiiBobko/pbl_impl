649ed8e32d8dcf142640a7f721dd0bdd3260017b
oleksii <oleksii@oleksii.debian.home>
2018-03-19 10:03:16 -0400

    initial commit

---
+++ lsh/src/main.c
@@ -0,0 +1,26 @@
+#include <cstdlib>
+#include <stdio.h>
+
+void lsh_loop(void)
+{
+  char *line;
+  char **args;
+  int status;
+
+  //do {
+    printf("> ");
+   // line = lsh_read_line();
+   // args = lsh_split_line(line);
+   // status = lsh_execute(args);
+
+   // free(line);
+    //free(args);
+//} while (status);
+}
+
+int main(int argc, char **argv)
+{
+    lsh_loop();
+    return EXIT_SUCCESS;
+}
+
